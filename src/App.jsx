5f76f502bf06f5f1e2598e661bc490db5c13a62b
alexakode <alexanderm.kodehode@gmail.com>
2025-09-29 11:05:23 +0200

    Working on rendering added users, rest of functionality done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import CookieClicker from "./components/CookieClicker";
 import CatFacts from "./components/CatFacts";
+import Users from "./components/Users"
 function App() {
   return (
     <div>
       <CookieClicker />
       <CatFacts />
+      <Users />
     </div>
   );
 }
---
+++ src/components/Users.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+const mockData = [
+  { username: "Ola Normann", email: "ola.normann@norge.no" },
+  { username: "Torleif", email: "torleif@kodehode.no" },
+  { username: "Jan Egil", email: "jan.egil@kodehode.no" },
+  { username: "Sander", email: "sander@kodehode.no" },
+];
+export default function listUsers() {
+  const [users, setUsers] = useState(mockData);
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+
+  const handleAddUser = () => {
+    if (!username || email) return;
+    const newUser = { username, email };
+    setUsers([...users, newUser]);
+    setUsername("");
+    setEmail("");
+  };
+
+  return (
+    <div>
+      <h2>Brukere</h2>
+      <ul>
+        {users.map((user, index) => (
+          <li key={index}>
+            {user.username} — {user.email}
+          </li>
+        ))}
+      </ul>
+      <h3>Legg til ny bruker</h3>
+      <input
+        type="text"
+        placeholder="Brukernavn"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="email"
+        placeholder="epost"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <button onClick={handleAddUser}>Legg til</button>
+    </div>
+  );
+}
