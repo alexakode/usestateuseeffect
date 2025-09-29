770024e7d92128dc2c599e7559462b8460772870
alexakode <alexanderm.kodehode@gmail.com>
2025-09-29 11:57:40 +0200

    Fixed slashes, page works

--- src/components/CookieClicker.jsx
+++ src/components/CookieClicker.jsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
 export default function CookieClicker() {
-  const[score, setScore] = useState(0);
+  const [score, setScore] = useState(0);
 
   return (
     <div className="cookie-clicker">
       <h2>Poeng: {score}</h2>
       <button onClick={() => setScore(score + 1)}>
-        <img src="/cookie-1.png" alt="Kjeks" width={112} />
+        <img src="cookie-1.png" alt="Kjeks" width={112} />
       </button>
     </div>
   );
