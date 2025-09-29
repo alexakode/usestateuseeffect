2edf69662c21f21567112f78f051dc8509f00faf
alexakode <alexanderm.kodehode@gmail.com>
2025-09-29 10:10:29 +0200

    Built minimal cookie clicker

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import CookieClicker from "./components/CookieClicker";
+function App() {
+  return (
+    <div>
+      <CookieClicker />
+    </div>
+  );
+}
+
+export default App;
